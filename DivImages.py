a2a5daa89de5b0d5172043f85b5fcb8762f08b37
SalvadorHellsing95 <jose.blanco@ugto.mx>
2018-06-05 10:33:00 -0500

    Se modificaron las divisiones de las imagenes

--- DivImages.py
+++ DivImages.py
@@ -13,15 +13,24 @@ except:
     print("No se ah podido cargar la imagen.")
     exit
 
-Reducida=imagen.resize((800,600)) #Se cambiaran los valores luego.
+Reducida=imagen.resize((1000,1000)) #Se cambiaran los valores luego.
 #Reducida.show()
 Reducida.save("Images/Reducida.jpg")
 Aux=0
 #Se empieza a recortar las imagenes.
-for i in range(10):
-    for j in range(10):    
+a=Reducida.size
+
+jump=20
+Size=100
+for i in range(0,(a[0]),jump):
+    for j in range(0,(a[1]),jump):
+        if (i+Size)>a[0]:
+            continue
+        if (j+Size)>a[1]:
+            continue
         NewDir='Images/ImageRec'
-        Box=list([80*j,60*i,80*(j+1),60*(i+1)])
+        Box=list([j,i,j+Size,i+Size])
+        #Box=list([20*j,20*i,20*(j+1),20*(i+1)])
         Box=tuple(Box)
         region=Reducida.crop(Box)
         NewDir+=str(Aux)
@@ -30,10 +39,11 @@ for i in range(10):
         Aux+=1
         
         #Prueba de Dibujo.
-        draw=ImageDraw.Draw(region)
+"""  draw=ImageDraw.Draw(region)
         draw.line((0, 0) + (0,59), fill=200)
         draw.line((0, 0) + (79,0), fill=200)
         draw.line((0, 59)+(79,59), fill=200)
         draw.line((79, 0)+(79,59), fill=200)
         del draw
         region.save(NewDir)
+"""
