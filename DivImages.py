a80e81e55470a99b77466e41680b4333e0abdbe1
SalvadorHellsing95 <jose.blanco@ugto.mx>
2018-06-07 13:15:03 -0500

    Prueba de dibujo de contorno

--- DivImages.py
+++ DivImages.py
@@ -13,15 +13,15 @@ except:
     print("No se ah podido cargar la imagen.")
     exit
 
-Reducida=imagen.resize((1000,1000)) #Se cambiaran los valores luego.
+Reducida=imagen #Se cambiaran los valores luego.
 #Reducida.show()
-Reducida.save("Images/Reducida.jpg")
+#Reducida.save("Images/Reducida.jpg")
 Aux=0
 #Se empieza a recortar las imagenes.
 a=Reducida.size
-
-jump=20
-Size=100
+Points=[]
+jump=10
+Size=50
 for i in range(0,(a[0]),jump):
     for j in range(0,(a[1]),jump):
         if (i+Size)>a[0]:
@@ -29,21 +29,28 @@ for i in range(0,(a[0]),jump):
         if (j+Size)>a[1]:
             continue
         NewDir='Images/ImageRec'
-        Box=list([j,i,j+Size,i+Size])
+        Box=list([i,j,i+Size,j+Size])
+        Points.append(Box);
         #Box=list([20*j,20*i,20*(j+1),20*(i+1)])
         Box=tuple(Box)
         region=Reducida.crop(Box)
         NewDir+=str(Aux)
         NewDir+='.jpg'
-        region.save(NewDir)
+        #region.save(NewDir)
         Aux+=1
         
-        #Prueba de Dibujo.
-"""  draw=ImageDraw.Draw(region)
-        draw.line((0, 0) + (0,59), fill=200)
-        draw.line((0, 0) + (79,0), fill=200)
-        draw.line((0, 59)+(79,59), fill=200)
-        draw.line((79, 0)+(79,59), fill=200)
+        
+SaveDraw='Images/Final.jpg'
+ImDraw=Image.open("Images/Ejemplo.jpg")
+Dra=0
+for Aux in Points:
+    if(Dra%100==0):
+    
+        draw=ImageDraw.Draw(ImDraw)
+        draw.line((Aux[0], Aux[1]) + (Aux[0],Aux[3]), fill=200)
+        draw.line((Aux[0], Aux[1]) + (Aux[2],Aux[1]), fill=200)
+        draw.line((Aux[0], Aux[3])+(Aux[2],Aux[3]), fill=200)
+        draw.line((Aux[2],Aux[3])+(Aux[2],Aux[1]), fill=200)
         del draw
-        region.save(NewDir)
-"""
+        ImDraw.save(SaveDraw)
+    Dra+=1
